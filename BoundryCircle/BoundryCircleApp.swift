b1346fdd673098f268c8e8acc9894d27da85fc7e
Jaylen Smith <kobe26945@icloud.com>
2023-02-12 01:11:42 -0500

    Initial Commit

---
+++ BoundryCircle/BoundryCircleApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoundryCircleApp.swift
+//  BoundryCircle
+//
+//  Created by Jaylen Smith on 2/12/23.
+//
+
+import SwiftUI
+
+@main
+struct BoundryCircleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BoundryCircle/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  BoundryCircle
+//
+//  Created by Jaylen Smith on 2/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
